d20e49328d1b804ce0d3bf12d7b83686d85f329e
wangxm <wangxm@zhen.com>
2016-09-27 01:55:05 +0800

    init
    Change-Id: I8686bec45bc646aa5cec17b9c1706b24e7ac4dda
    Signed-off-by: wangxm <wangxm@zhen.com>

---
+++ study/src/main/java/com/gray/user/service/UserService.java
@@ -0,0 +1,7 @@
+package com.gray.user.service;
+
+import com.gray.user.entity.User;
+
+public interface UserService {
+	boolean doUserLogin(User user);
+}
---
+++ study/src/main/java/com/gray/user/service/impl/UserServiceImpl.java
@@ -0,0 +1,28 @@
+package com.gray.user.service.impl;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.gray.user.dao.UserDao;
+import com.gray.user.entity.User;
+import com.gray.user.service.UserService;
+@Service
+public class UserServiceImpl implements UserService {
+	@Autowired
+	private UserDao<User> dao;
+	
+	public boolean doUserLogin(User user) {
+		List<User> list = dao.selectId(user.getUsername());
+		if(list.size() == 0){
+			return false;
+		}else{
+			if(list.get(0).getPassword().equals(user.getPassword())){				
+				return true;
+			}else{
+				return false;
+			}			
+		}
+	}	
+}
